5eca223526484870e2b5c12042407a4b6433d07d
cheche0208 <jmonterrey1993@gmail.com>
2018-08-06 13:28:49 -0400

    Jose

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->enum('type_document_emp',['V','E','G','J']);
+            $table->integer('nro_document_emp');
+            $table->string('name_emp');
+            $table->string('tlf_local_emp');
+            $table->string('tlf_movil_emp');
+            $table->date('fecha_contrato_ini');
+            $table->date('fecha_contrato_final');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('Activo',['Disponible', 'Inactivo'])->default('Disponible');
             $table->rememberToken();
             $table->timestamps();
         });
