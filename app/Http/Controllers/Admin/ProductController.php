1879b7b0a070e32b3a54e7e0db54e679ccd6a905
cheche0208 <jmonterrey1993@gmail.com>
2018-08-07 09:19:34 -0400

    Jose

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Product;
+use App\Category;
+use App\User;
 
 class ProductController extends Controller
 {
@@ -11,74 +15,95 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    ublic function index()
     {
-        //
+        $products = Product::orderBy('id', 'DESC')->paginate(5);
+
+        return view('admin.products.index', compact('products'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+/**
+ * Show the form for creating a new resource.
+ *
+ * @return \Illuminate\Http\Response
+ */
     public function create()
     {
-        //
+        $categories = Category::orderBy('name', 'ASC')->pluck('name', 'id');
+      
+
+        return view('admin.products.create', compact('categories'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+/**
+ * Store a newly created resource in storage.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @return \Illuminate\Http\Response
+ */
+    public function store(ProductStoreRequest $request)
     {
-        //
-    }
+        $product = Product::create($request->all());
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+        return redirect()->route('products.edit', $product->id)->with('info', 'Producto creado con éxito');
+    }
+/**
+ * Display the specified resource.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+        $category_id = Category::find($id);
+        
+
+        return view('admin.products.show', compact('product', 'category_id'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+/**
+ * Show the form for editing the specified resource.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
     public function edit($id)
     {
-        //
+        $categories = Category::orderBy('name', 'ASC')->pluck('name', 'id');
+       
+        $product    = Product::find($id);
+
+        return view('admin.products.edit', compact('product', 'categories'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+/**
+ * Update the specified resource in storage.
+ *
+ * @param  \Illuminate\Http\Request  $request
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
+    public function update(ProductUpdateRequest $request, $id)
     {
-        //
+        $product = Product::find($id);
+
+        $product->fill($request->all())->save();
+
+        return redirect()->route('products.edit', $product->id)->with('info', 'Producto actualizado con éxito');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+/**
+ * Remove the specified resource from storage.
+ *
+ * @param  int  $id
+ * @return \Illuminate\Http\Response
+ */
     public function destroy($id)
     {
-        //
+        $product = Product::find($id)->delete();
+
+        return back()->with('info', 'Eliminado correctamente');
     }
+
 }
--- app/Product.php
+++ app/Product.php
@@ -6,19 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    
     protected $fillable = [
-        'code','name','stock' ,'precio' ,'user_id' ,'category_id',
+        'code', 'name', 'stock', 'precio', 'category_id',  'user_id',
     ];
 
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
+  
 
     public function users()
     {
         return $this->belongsTo(User::class);
     }
+
+   
 
 }
